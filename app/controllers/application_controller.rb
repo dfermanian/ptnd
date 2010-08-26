4bf62b85ec0a25ec9aae13b920ed1cb01bf8bf19
Daniel Fermanian <dfermanian@gmail.com>
2010-08-26 11:31:12 -0700

    modified application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
+#---
+# Excerpted from "Developing Facebook Platform Applications with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/mmfacer for more book information.
+#---
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery  :secret => 'a7cabcdf1499df9ded55d8a3797d9387'
+  ensure_authenticated_to_facebook
 end
