291b365715b396a1fb12a98ecbda63177c879ceb
Daniel Fermanian <dfermanian@gmail.com>
2010-08-26 10:56:02 -0700

    first of the day ;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   
-  before_filter :set_facebook_session
+  #before_filter :set_facebook_session
   helper_method :facebook_session
 end
