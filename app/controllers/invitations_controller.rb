b4d5c789f6e15cac5d68246ed001d5168370242a
Daniel Fermanian <dfermanian@gmail.com>
2010-08-31 11:10:51 -0700

    added button to new.erb

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,23 +9,8 @@
 class InvitationsController < ApplicationController
   
   def new
-  	if should_update_profile?
-  	  update_profile
-  	end
   	@from_user_id = facebook_session.user.to_s
   end
-  
-  def should_update_profile?
-    params[:from]
-  end
-  
-  def update_profile
-  	@user = facebook_session.user
-  	@user.profile_fbml = 
-  	render_to_string(:partial=>"profile",
-  	  :locals=>{:from=>params[:from]})	  
-  end
-  
   
   def create
     @sent_to_ids = params[:ids]
