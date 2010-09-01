1e53ffad06f2350b234f291ca414797ea262e752
Daniel Fermanian <dfermanian@gmail.com>
2010-09-01 15:09:20 -0700

    dealing with the string/int thing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base 
   
   def self.for(facebook_id,facebook_session=nil)
-    returning find_or_create_by_facebook_id(facebook_id) do |user|
+    returning find_or_create_by_facebook_id(facebook_id.to_i) do |user|
       unless facebook_session.nil?
         user.store_session(facebook_session.session_key) 
       end
