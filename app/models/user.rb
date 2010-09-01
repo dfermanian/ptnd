b367df3d264d21823e462264390b86f2711bf558
Daniel Fermanian <dfermanian@gmail.com>
2010-09-01 15:21:45 -0700

    dealing with the string/int thing 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base 
   
   def self.for(facebook_id,facebook_session=nil)
-    returning find_or_create_by_facebook_id(facebook_id.to_i) do |user|
+    returning find_or_create_by_facebook_id(facebook_id) do |user|
       unless facebook_session.nil?
         user.store_session(facebook_session.session_key) 
       end
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   def facebook_session
     @facebook_session ||= 
       returning Facebooker::Session.create do |session|
-        session.secure_with!(session_key, facebook_id.to_i, 1.hour.from_now)
+        session.secure_with!(session_key, facebook_id, 1.hour.from_now)
       end
   end
   
