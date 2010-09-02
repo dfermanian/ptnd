dd292a498badcb3a37cd65f951d01fc1e66f8c7b
Daniel Fermanian <dfermanian@gmail.com>
2010-09-02 11:28:19 -0700

    development log

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100901183950) do
+ActiveRecord::Schema.define(:version => 20100901225127) do
 
   create_table "assignments", :force => true do |t|
     t.datetime "created_at"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(:version => 20100901183950) do
 
   create_table "hellos", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "moves", :force => true do |t|
+    t.string   "name"
+    t.string   "image_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
